dd88025c327d210fcdb2656826dd098871e642c7
Reina630 <89133502+Reina630@users.noreply.github.com>
2026-02-26 09:25:40 +0100

    fichiers statics

--- ged/settings.py
+++ ged/settings.py
@@ -147,7 +147,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Custom User Model
 AUTH_USER_MODEL = 'users.User'
